6b5665d26fc837ddb43105500ad79fd73bd4179c
Prajwal Waykos <90028385+Praj-17@users.noreply.github.com>
2023-03-01 15:28:42 +0530

    added basic main skeleton

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,8 @@
 import sys
 import cv2
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout,QHBoxLayout
 from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtCore import QTimer
 
 class Dashboard(QWidget):
     def __init__(self):
@@ -14,24 +15,55 @@ class Dashboard(QWidget):
         # Create a layout to organize the label
         layout = QVBoxLayout()
         layout.addWidget(self.label)
-        
+
+        # Create a layout for the side panel
+        side_panel_layout = QVBoxLayout()
+
+        # Define the font and text to be displayed
+        self.font = cv2.FONT_HERSHEY_SIMPLEX
+        self.doubted = 0
+        self.total = 0
+        self.leaky = 0
+        self.verified = 0
+
+        # Add labels for each variable and its value to the side panel
+        self.doubted_label = QLabel('Doubted: {}'.format(self.doubted))
+        self.total_label = QLabel('Total: {}'.format(self.total))
+        self.leaky_label = QLabel('Leaky: {}'.format(self.leaky))
+        self.verified_label = QLabel('Verified: {}'.format(self.verified))
+        side_panel_layout.addWidget(self.doubted_label)
+        side_panel_layout.addWidget(self.total_label)
+        side_panel_layout.addWidget(self.leaky_label)
+        side_panel_layout.addWidget(self.verified_label)
+
+        # Create a horizontal layout to hold the video frame and the side panel
+        horizontal_layout = QHBoxLayout()
+        horizontal_layout.addWidget(self.label)
+        horizontal_layout.addLayout(side_panel_layout)
+
         # Set the layout for the widget
+        layout.addLayout(horizontal_layout)
         self.setLayout(layout)
         
         # Initialize the video capture object
         self.cap = cv2.VideoCapture(0)
-        
-        # Define the font and text to be displayed
-        self.font = cv2.FONT_HERSHEY_SIMPLEX
-        self.text = 'Hello, World!'
-        
+
+        self.update_side_panel()
+
+    def update_side_panel(self):
+        # Update the values displayed in the side panel
+        self.doubted_label.setText('Doubted: {}'.format(self.doubted))
+        self.total_label.setText('Total: {}'.format(self.total))
+        self.leaky_label.setText('Leaky: {}'.format(self.leaky))
+        self.verified_label.setText('Verified: {}'.format(self.verified))
+
     def update_frame(self):
         # Read a frame from the video capture object
         ret, frame = self.cap.read()
         
         if ret:
             # Add text to the frame
-            cv2.putText(frame, self.text, (50, 50), self.font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.putText(frame, 'Hello, World!', (50, 50), self.font, 1, (255, 255, 255), 2, cv2.LINE_AA)
             
             # Convert the frame to a QImage
             rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 def leak_detection(image):
+    global total
     print("Leak detection started")
     return True
 def hydrophone_verification():
@@ -11,4 +12,62 @@ def identify_leaky_cylinder():
 
 if __name__ == "__main__":
     cam = cv2.VideoCapture(0)
-    
+    total = 0
+    doubted = 0
+    leaky = 0
+    verified = 0
+    while cam.isOpened():
+        try:
+            ret, frame = cv2.imread()
+        except Exception as e:
+            ret = None
+            frame = None
+            print("Exception", e)
+        if ret == False or frame is None:
+            print("Camera Frame cannot be loaded")
+            break
+        
+        try:
+            is_leaky = leak_detection(frame)
+        except Exception as e:
+            is_leaky = False
+            print("Exception", e)
+        
+        if is_leaky == True:
+            doubted +=1
+            
+            #Going for Hydrophone verfication
+            try:
+                hydrophone_verification_flag = hydrophone_verification()
+                verified +=1
+            except Exception as e:
+                is_leaky_hydrophone = False
+                print("Exception", e)
+            
+            if hydrophone_verification_flag == True:
+                leaky +=1
+                
+                #Going For identification of Leaky Cylinder
+                try:
+                    n = identify_leaky_cylinder()
+                except Exception as e:
+                    print("Excpetion", e)
+                    n = 0
+                print("Leaky cylinder at ", n)
+            else:
+                pass
+                
+            
+            
+        else:
+            pass #continue the while loop
+        
+                
+            
+            
+        
+        
+        
+        
+
+            
