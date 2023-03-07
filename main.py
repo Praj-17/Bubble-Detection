7a1079fe66a72cbe65b754958fa69936760656ff
Srushti Shevate <srushtishevate9@gmail.com>
2023-03-07 23:27:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,36 +12,41 @@ def identify_leaky_cylinder():
     print("identifying leaky cylinder")
     return 1
 
-def add_logo(image, logo, pos_y, pos_x):
-     # Add the logo image to the video frame
+def add_logo(image, logo, pos_y, pos_x, pos_y2=None, pos_x2=None):
     if isinstance(image, str) and os.path.exists(image):
         image = cv2.imread(image)
     if isinstance(logo, str) and os.path.exists(logo):
         logo = cv2.imread(logo)
-        print(logo)
-    # Read logo and resize
     size = 70
     logo = cv2.resize(logo, (size, size))
-    # Create a mask of logo
     img2gray = cv2.cvtColor(logo, cv2.COLOR_BGR2GRAY)
     ret, mask = cv2.threshold(img2gray, 1, 255, cv2.THRESH_BINARY)
-    # Set ROI for logo placement
     roi = image[10:size+10, 10:size+10]
-  
-    # Set an index of where the mask is
     roi[np.where(mask)] = 0
     roi += logo
-     
-    # alpha = 0.5
+    
+    if pos_y2 is not None and pos_x2 is not None:
+        size = 70
+        logo = cv2.resize(logo, (size, size))
+        img2gray = cv2.cvtColor(logo, cv2.COLOR_BGR2GRAY)
+        ret, mask = cv2.threshold(img2gray, 1, 255, cv2.THRESH_BINARY)
+        roi = image[10:size+10, image.shape[1]-80-size+70:image.shape[1]-80+70]
+        roi[np.where(mask)] = 0
+        roi += logo
+
+         # alpha = 0.5
     # beta = 1 - alpha
     # overlay = cv2.addWeighted(image[pos_y:pos_y+logo.shape[0], pos_x:pos_x+logo.shape[1]], alpha, logo, beta, 0)
     # image[pos_y:pos_y+logo.shape[0], pos_x:pos_x+logo.shape[1]] = overlay
     return image
 
+   
+
 def put_data_on_image(image, total, doubted, leaky, verified):
     
     if isinstance(image, str) and os.path.exists(image):
         image = cv2.imread(image)
+        
     else:
         text1 = f" Total: {total}"
         text2 = f" Doubted: {doubted}"
@@ -50,27 +55,60 @@ def put_data_on_image(image, total, doubted, leaky, verified):
         heading = "Automatic Leak Detection"
         
         base_x = 950
-        base_y = 450
-        
+        base_y = 380
+       
         #Rectangele Behind Headings
-        cv2.rectangle(image, (0, 0),(1550, 80),(250,255,208), thickness=-1 )
+        cv2.rectangle(image, (0, 0),(1550, 80),(255,255,255), thickness=-1 )
         #Heading
-        cv2.putText(image, heading, (280,50), cv2.FONT_HERSHEY_COMPLEX, 1.8, (0,0,0), thickness=2)
-        image = add_logo(image, "Bharat-Petroleum-logo.png", 20,20)
+        cv2.putText(image, heading, (270,58), cv2.FONT_HERSHEY_COMPLEX, 1.8, (0,0,0), thickness=2)
+
+        image = add_logo(image, "download.png", 10,10)
+        image = add_logo(image, "download.png", 10,10, 10, image.shape[1]-160)
+
         
-        
-        #Data Rectangle
-        cv2.rectangle(image, (base_x+10, base_y-140),(base_x + 350, base_y+180),(255,253,208), thickness=-1 )
+        #Data right hand side box
+        cv2.rectangle(image, (base_x+10, base_y-300),(base_x + 350, base_y+350),(133,100,9), thickness=-1 )
       
-        cv2.putText(image, "Auto", (base_x+100,base_y-80), cv2.FONT_HERSHEY_COMPLEX, 2, (0,255,255), thickness=2)
-        
-        cv2.putText(image, text1, (base_x,base_y), cv2.FONT_HERSHEY_COMPLEX, 1, (255,0,0), thickness=2)
-        cv2.putText(image, text2, (base_x,base_y + 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255,0,0), thickness=2)
-        cv2.putText(image, text3, (base_x,base_y + 100), cv2.FONT_HERSHEY_COMPLEX, 1, (255,0,0), thickness=2)
-        cv2.putText(image, text4, (base_x,base_y + 150), cv2.FONT_HERSHEY_COMPLEX, 1, (255,0,0), thickness=2)
+        cv2.rectangle(image, (base_x+30, base_y-130), (base_x+150, base_y-90), (255, 255, 255), -1)
+        cv2.rectangle(image, (base_x+30, base_y-130), (base_x+150, base_y-90), (0, 0, 0), 2)
+        cv2.rectangle(image, (base_x+30, base_y-70), (base_x+200, base_y-30), (255, 255, 255), -1)
+        cv2.rectangle(image, (base_x+30, base_y-70), (base_x+200, base_y-30), (0, 0, 0), 2)
+        cv2.rectangle(image, (base_x+30, base_y-10), (base_x+160, base_y+30), (255, 255, 255), -1)
+        cv2.rectangle(image, (base_x+30, base_y-10), (base_x+160, base_y+30), (0, 0, 0), 2)
+        cv2.rectangle(image, (base_x+30, base_y+50), (base_x+260, base_y+90), (255, 255, 255), -1)
+        cv2.rectangle(image, (base_x+30, base_y+50), (base_x+260, base_y+90), (0, 0, 0), 2)
+
+        cv2.putText(image, "Auto", (base_x+100,base_y-180), cv2.FONT_HERSHEY_COMPLEX, 2, (0,0,0), thickness=2)
+        cv2.putText(image, text1, (base_x+30,base_y-100), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,0), thickness=2)
+        cv2.putText(image, text2, (base_x+30,base_y-40), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,0), thickness=2)
+        cv2.putText(image, text3, (base_x+30,base_y + 20), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,0), thickness=2)
+        cv2.putText(image, text4, (base_x+25,base_y+80), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,0), thickness=2)
+
+
+        #draw the lower left corner box
+        box_height = 100
+        box_width = 957
+        box_thickness = 2
+        box_color = (133,100,9)
+        cv2.rectangle(image, (1, image.shape[0] - box_height - 1), (1 + box_width, image.shape[0] - 1), box_color, thickness=box_thickness)
+        cv2.rectangle(image, (1, image.shape[0] - box_height - 1), (1 + box_width, image.shape[0] - 1), box_color, thickness= -1)
+
+        text = 'Error status: '
+        font_scale = 0.9
+        font_thickness = 2
+        text_color = (0, 0, 0)
+        cv2.putText(image, text, (30, 650), cv2.FONT_HERSHEY_SIMPLEX, font_scale, text_color, font_thickness)
+
         return image
         
-        
+def error_status(img, text, pos, font=cv2.FONT_HERSHEY_SIMPLEX, font_scale=1, color=(255, 255, 255), thickness=2, error_status=None):
+    cv2.putText(img, text, pos, font, font_scale, color, thickness)
+    if error_status is not None:
+        (text_width, text_height), _ = cv2.getTextSize(error_status, font, font_scale, thickness)
+        error_pos = (pos[0], pos[1] + text_height + 10)
+        cv2.rectangle(img, error_pos, (error_pos[0] + text_width, error_pos[1] + text_height + 10), color, cv2.FILLED)
+        cv2.putText(img, error_status, error_pos, font, font_scale, (0, 0, 0), thickness)
+    
     
 
 if __name__ == "__main__":
@@ -88,6 +126,7 @@ if __name__ == "__main__":
             print("Exception", e)
         if ret == False or frame is None:
             print("Camera Frame cannot be loaded")
+            error_status(frame, 'Camera Frame cannot be loaded',(20, 50), error_status='Error')
             break
         
         try:
@@ -131,12 +170,4 @@ if __name__ == "__main__":
         else:
             pass #continue the while loop
         
-                
-            
-            
-        
-        
-        
-        
-
-            
+     
